e3dccf7ef8a91c196a960fc35d8821e6a517eb97
aminefjj <amine.ferjeni2001@gmail.com>
2026-01-21 10:39:07 +0100

    vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,6 @@ import ParkingList from "@/components/ParkingList/ParkingList";
 import { LoadingSpinner } from "@/components/ui/loadingspinner";
 import ParkingListFilters from "@/components/ParkingList/ParkingListFilters";
 import { useParkingSearchStore } from "@/stores/parkingSearchStore";
-{import.meta.env.VITE_APP_VERSION}
 
 function App() {
   const { parkingName } = useParkingSearchStore();
@@ -22,7 +21,7 @@ function App() {
   return (
     <div className="flex flex-col gap-5 items-center">
       <h1 className="text-2xl font-bold text-center">
-        Where can I Park in Angers ? 👀
+        Where can I Park in Angers ? 👀 {VITE_APP_VERSION}
       </h1>
       <ParkingListFilters
         onChange={(parkingName: string) => {
